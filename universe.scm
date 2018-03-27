ca65a4c74e47b8037fa8e935932d3e9c79d6028e
Brock Jones <brawk212@gmail.com>
2018-03-27 18:44:36 -0400

    More macro stuff

--- universe.scm
+++ universe.scm
@@ -1,5 +1,22 @@
+(define-syntax test 
+  (syntax-rules () 
+		((test world body ...)
+		 (begin (define the-world world) (big-bang body ...)))))
+
 (define-syntax big-bang
   (syntax-rules ()
-	       ((_ (name other) ...)
-		(begin (cond [(memq (quote name) '(on-draw stop-when on-key)) 
-						    (define name other)]) ...))))
+		((_ (name other) ...)
+		 (begin (cond [(memq (quote name) '(on-draw stop-when on-key)) 
+				     (define name other)]) ... ))))
+
+(define draw (lambda ()
+	       (begin
+		 (display world)
+		 (display on-draw))))
+
+;(define-syntax let2
+;  (syntax-rules (on-draw)
+;		((let2 (on-draw other) body ...)
+;		 (let ([draw other]) (let2 body ...)))
+;		((let2) (display draw))))
+
