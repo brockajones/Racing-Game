91562ae2809c8fe6c19aba02c9357f9aa650d61e
Brock Jones <brawk212@gmail.com>
2018-03-27 22:25:32 -0400

    Improved big-bang

--- universe.scm
+++ universe.scm
@@ -1,18 +1,37 @@
-(define-syntax test 
+(use (prefix sdl2 sdl2:)
+     (prefix sdl2-ttf ttf:))
+
+(define on-draw #f)
+(define stop-when #f)
+(define on-key #f)
+
+
+(define-syntax big-bang 
   (syntax-rules () 
-		((test world body ...)
-		 (begin (define the-world world) (big-bang body ...)))))
+		((test a-world body ...)
+		 (begin (define world a-world) (sub-bang body ...)))))
 
-(define-syntax big-bang
+(define-syntax sub-bang
   (syntax-rules ()
-		((_ (name other) ...)
+		((_ (name other ...) ...)
 		 (begin (cond [(memq (quote name) '(on-draw stop-when on-key)) 
-				     (define name other)]) ... ))))
+				     (define name (list other ... ))]) ... (start)))))
 
-(define draw (lambda ()
-	       (begin
-		 (display world)
-		 (display on-draw))))
+(define start (lambda ()
+		(sdl2:set-main-ready!)
+		(sdl2:init! '(video))
+		(ttf:init!)
+		(current-exception-handler
+		  (let ((original-handler (current-exception-handler)))
+		    (lambda (exception)
+		      (sdl2:quit!)
+		      (original-handler exception))))
+		(let ([window-size (cond [(= (length on-draw) 3) (cdr on-draw)]
+					 [else '(640 480)])])
+		(define-values (*window* *render*) (sdl2:create-window-and-renderer! 
+						     (car window-size) (cadr window-size) '())))
+		(run)))
+
 
 ;(define-syntax let2
 ;  (syntax-rules (on-draw)
