b2095a428b43c40755596eb3d80d2bccddbedf96
Brock Jones <brawk212@gmail.com>
2018-04-09 01:12:14 -0400

    Failed idea #1

--- main.scm
+++ main.scm
@@ -1,9 +1,10 @@
 (load "universe.scm")
 
-(big-bang (init-world 1)
+(big-bang 1
 	  (on-draw (lambda (x y) (sdl2:render-draw-color-set! (cdr y) (sdl2:make-color 200 255 255))
+		     (display "hi")
 		     (sdl2:render-fill-rect! (cdr y) (sdl2:make-rect (floor x) (floor x) 600 400))
-		     (sdl2:render-draw-color-set! (cdr y) (sdl2:make-color 0 0 0))
-		     x))
-	  (stop-when (lambda (x) (> x 500)))
-	  (on-key (lambda (world event) (+ 0.5 world))))
+		     (sdl2:render-draw-color-set! (cdr y) (sdl2:make-color 0 0 0)) (display x)
+		     (+ 1 x))))
+	  ;(stop-when (lambda (x) (> x 500)))
+	  ;(on-key (lambda (world event) (+ 0.5 world))))
--- universe.scm
+++ universe.scm
@@ -3,16 +3,10 @@
 (use srfi-113)
 (use srfi-128)
 
-(define-record state world sdl return events)
-;(define (make-state world sdl return events) 
-;  (alist->hash-table `((world . ,world) 
-;		       (sdl . ,sdl)
-;		       (return . ,return)
-;		       (events . ,events))))
 (define-syntax make-hash 
-     (syntax-rules ()
-            ((_ (key val) ... ) 
-	     (alist->hash-table (list (cons (quote key) val) ...)))))
+  (syntax-rules ()
+		((_ (key val) ... ) 
+		 (alist->hash-table (list (cons (quote key) val) ...)))))
 
 
 (define-record events keyboard)
@@ -36,7 +30,40 @@
 			(cond [(null? events) world]
 			      [else (proc (do-key-events world (cdr events) proc) (car events))])))
 
-(define-syntax big-bang 
+(define-syntax big-bang
+  (syntax-rules ()
+		((_ init-world (id args ...) ...)
+		 (begin
+		   (sdl2:set-main-ready!)
+		   (sdl2:init!)
+		   (ttf:init!)
+		   (current-exception-handler
+		     (let ((original-handler (current-exception-handler)))
+		       (lambda (exception)
+			 (sdl2:quit!)
+			 (original-handler exception))))
+		   (let* ( [window-size '(1280 720)]
+			  [sdl (call-with-values 
+				 (lambda () (sdl2:create-window-and-renderer!  1280 720 '()))
+				 cons)]
+			  [events (make-events (make-set))])
+		     (letrec ([do-action (lambda (a-id lst)
+				       (apply (hash-table-ref handler a-id) lst))]
+			      [handler (make-hash (on-draw 
+			      (lambda (proc)
+				      (sdl2:render-clear! (cdr sdl))
+				      (let ([result (proc ...)])
+					(sdl2:render-present! (cdr sdl)) result)))
+			   (stop-when (lambda (proc) (cond [(proc world)
+								   (return world)]))))]
+			   [run (lambda (world)
+				       (call/cc (lambda (return)
+						  (do-action (quote id) (list args ...))
+						  (run init-world))))]) 
+			 (run init-world)))
+		     (sdl2:destroy-window! (car sdl))))))
+
+(define-syntax big-bang2 
   (syntax-rules (init-world on-draw stop-when on-key)
 		((_ (init-world val) body body* ...)
 		 (begin 
@@ -57,19 +84,19 @@
 				     (call/cc (lambda (return)
 						(set! events (get-events events))
 						(let* ([state (make-state world sdl return events)])
-						  (run (big-bang state body body* ...) )))))]) 
+						  (run (big-bang2 state body body* ...) )))))]) 
 		       (run val))
 		     (sdl2:destroy-window! (car sdl)))))
 		((_ state (on-draw proc) body ...)
 		 (begin 
 		   (sdl2:render-clear! (cdr (state-sdl state)))
-		   (let ([result (proc (big-bang state body ...) (state-sdl state))])
+		   (let ([result (proc (big-bang2 state body ...) (state-sdl state))])
 		     (sdl2:render-present! (cdr (state-sdl state))) result)))
 		((_ state (stop-when proc) body ...)
-		 (let ([return (state-return state)]) (cond [(proc (big-bang state body ...)) 
+		 (let ([return (state-return state)]) (cond [(proc (big-bang2 state body ...)) 
 							     (return (state-world state))]) 
-		   (big-bang state body ...)))
+		   (big-bang2 state body ...)))
 		((_ state (on-key proc) body ...)
-		 (do-key-events (big-bang state body ...) (set->list (events-keyboard (state-events state))) proc))
+		 (do-key-events (big-bang2 state body ...) (set->list (events-keyboard (state-events state))) proc))
 		((_ state)
 		 (state-world state))))
