f07d4751119f7b23c5d5fed1bd3d211eef76df74
brockajones <brawk212@gmail.com>
2018-04-30 01:11:51 -0400

    Another attempt at bounce

--- main.scm
+++ main.scm
@@ -20,25 +20,29 @@
 (define tup-map (lambda (proc tup)
 		  (cons (proc (car tup)) (proc (cdr tup)))))
 
+(define fix-angle (lambda (rad)
+		    (cond [(> rad (* 2 pi)) (fix-angle (- rad (* 2 pi)))]
+			  [(< rad 0) (+ (* 2 pi) rad)]
+			  [else rad])))
+
+(define rad->deg (lambda (rad)
+		   (* rad (/ 180 pi))))
+
 (define angle->vec (lambda (d l)
 		     (cons (round (* l (cos d))) 
 			   (round (* l (sin d))))))
 (define reflection (lambda (vec line d-line)
-
 		     (let* ([line-angle (atan (-(second line) (fourth line))
 					      (- (first line) (third line)))]
-			    [d-angle (atan (- (second d-line) (fourth d-line))
-					   (- (first d-line) (third d-line)))]
+			    [d-angle (atan (-  (fourth d-line) (second d-line))
+					   (-  (third d-line) (first d-line)))]
 			    [vec-angle (atan (cdr vec) (car vec))]
 			    [vec-length (distance 0 0 (car vec) (cdr vec))]
-			    [new-angle (- line-angle (abs (- line-angle vec-angle)))]
-			    [final-angle (if (equal? (angle->vec new-angle vec-length)
-						     (tup-map exact->inexact vec))
-					   (+ line-angle (abs (- line-angle vec-angle)))
-					   new-angle)])
+			    [final-angle (+ d-angle (- d-angle (fix-angle (+ pi vec-angle))))])
 		       (set! angle-a vec-angle)
-		       (cond [(< (/ pi 2) (abs (- d-angle vec-angle))) vec]
-			     [(angle->vec final-angle vec-length)]))))
+		       (set! angle-b d-angle)
+		       (cond [(>= (/ pi 2) (abs (- vec-angle d-angle))) vec]
+			     [else (angle->vec final-angle vec-length)]))))
 
 (define pi 3.141592)
 
@@ -83,6 +87,7 @@
 		     [else world]))))
 
 (define angle-a 0)
+(define angle-b 0)
 
 (big-bang (init-world (lambda (sdl) (make-hash 
 				      (track '((640 0 0 360) (640 0 1280 360) (1280 360 640 720)
@@ -102,8 +107,10 @@
 	  (on-draw (lambda (world sdl) 
 		     (let* ( [c (hue->rgb (floor (hash-ref world 'color)))])
 		       (set-color sdl (invert c))
-		       (let ([vec-a (angle->vec angle-a 50)])
-			 (draw-line sdl 640 360 (+ 640 (car vec-a)) (+ 360 (cdr vec-a))))
+		       (let ([vec-a (angle->vec angle-a 50)]
+			     [vec-b (angle->vec angle-b 100)])
+			 (draw-line sdl 640 360 (+ 640 (car vec-a)) (+ 360 (cdr vec-a)))
+			 (draw-line sdl 640 360 (+ 640 (car vec-b)) (+ 360 (cdr vec-b))))
 		       (render-track world sdl)
 		       (bounce world sdl)
 		       (let ([return
